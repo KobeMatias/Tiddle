9c90c3228182635d2e4ff860adac2894ea74a2b9
Kobe Matias <kobematias@yahoo.com>
2020-08-09 17:25:25 -0400

    working commit

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron text-center">
+            <h1>Google Books Search</h1>
+            <h3>Search and Save Books</h3>
+            <h4>Powered by React</h4>
+        </div>
+    );
+}
+
+export default Jumbotron;
