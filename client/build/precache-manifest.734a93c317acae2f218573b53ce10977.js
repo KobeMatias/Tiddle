0b17e5e2af9bd03c53b96f64125bcd0dd59cd1ca
Kobe Matias <kobematias@yahoo.com>
2020-08-09 18:03:04 -0400

    again

---
+++ client/build/precache-manifest.734a93c317acae2f218573b53ce10977.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1db279cb222459a32981",
+    "url": "/static/css/main.883368c6.chunk.css"
+  },
+  {
+    "revision": "1db279cb222459a32981",
+    "url": "/static/js/main.24df7643.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "790670d6245215246ed7",
+    "url": "/static/js/2.a683230d.chunk.js"
+  },
+  {
+    "revision": "0d25baec2f80efa48ba206c750bf9e19",
+    "url": "/index.html"
+  }
+];
